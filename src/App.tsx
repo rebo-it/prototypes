69616d57b82c0362032e68178ff3f61ad27a01cc
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2024-08-28 19:33:40 -0600

    Added main branch trigger

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
           <br/>
-          Testing...
+          Main branch.
         </p>
         <a
           className="App-link"
