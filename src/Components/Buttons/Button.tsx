146d895e4c5b70485fa9fd049032866cdc365e3f
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-09-03 18:09:41 -0600

    created header

--- src/Components/Buttons/Button.tsx
+++ src/Components/Buttons/Button.tsx
@@ -1,7 +1,41 @@
 import React from "react";
-
+import './Button.css'; 
 interface ButtonProps {
     label: string;
-    onClick: () => void;
-    variant?: 'primary' | 'secondary';
+    variant?: 'HeaderButton' | 'GetStarted'| 'Apply' | 'LearnMore';
+    size?: 'small' | 'medium' | 'large';
+  
 }
+const Button: React.FC<ButtonProps> = ({ label,  variant, size}) => {
+    const getButtonClass = () => {
+        let baseClass = 'btn';
+        switch (variant) {
+            case 'HeaderButton':
+            baseClass += ' btn-header';
+            break;
+            case 'GetStarted':
+            baseClass += ' btn-get-started';
+            break;
+            case 'Apply':
+            baseClass += ' btn-apply';
+            break;
+            case 'LearnMore':
+            baseClass += ' btn-learn-more';
+            break;
+            default:
+            break;
+        }
+        if (size === 'small') baseClass += ' btn-small';
+        else if (size === 'large') baseClass += ' btn-large';
+        else if (size === 'medium') baseClass += ' btn-medium';
+
+        return baseClass;
+    };
+    return (
+        <button className={getButtonClass()} >
+          {label}
+        </button>
+      );
+    };
+    
+    export default Button;
