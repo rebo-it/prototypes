d2c55301cf9bc64d1c8c27886af4cec0d4200b99
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-09-16 21:00:48 -0600

    Created WIP page

--- src/Components/WIP_page/Work_in_progress.tsx
+++ src/Components/WIP_page/Work_in_progress.tsx
@@ -5,7 +5,9 @@ const WorkInProgress: React.FC = () => {
 
   return(
 		<div>
+			<p>Lo sentimos, actualmente este sitio web se encuentra bajo construcción</p>
 			
+			<div></div>
 
 		</div>
   );
