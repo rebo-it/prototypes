c0027dc36225dc911430c46bb47055a98d474917
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-09-17 17:32:38 -0600

    WIP page adjustments

--- src/Components/WIP_page/Work_in_progress.tsx
+++ src/Components/WIP_page/Work_in_progress.tsx
@@ -5,11 +5,14 @@ const WorkInProgress: React.FC = () => {
 
   return(
 		<div className="WipContainer">
-			<p className="WipText">Lo sentimos, actualmente este sitio web se encuentra bajo construcción</p>
+			<p className="WipText">Este sitio web se encuentra bajo construcción</p>
 			
-			<div>
-				Aquí los contactos:
-				<p> <i className="bi bi-instagram"></i> </p>
+			<div className="DatosDeContacto">
+				<p>Podrás encontrarnos en los siguientes enlaces:</p>
+				<p> <a href="https://www.instagram.com/" target="_blank"> <i className="bi bi-instagram"style={{ fontSize: '50px', color: 'white' }}  >  </i></a> </p>
+				<p> <a href="https://www.instagram.com/" target="_blank"> <i className="bi bi-envelope"style={{ fontSize: '50px', color: 'white' }}  >  </i></a> </p>
+				<p> <a href="https://web.whatsapp.com/" target="_blank"> <i className="bi bi-whatsapp"style={{ fontSize: '50px', color: 'white' }}  > </i></a> </p>
+				
 				
 			</div>
 
