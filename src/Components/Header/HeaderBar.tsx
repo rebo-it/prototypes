b2ca9cb6d65246f565aa20cf907500ab5d0c59bd
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-09-20 19:49:01 -0600

    Created WIP branch and created header component

---
+++ src/Components/Header/HeaderBar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './HeaderBar.css';
+
+interface HeaderBarprops{
+  
+}
+const HeaderBar: React.FC<HeaderBarprops> = () => {
+
+	return (
+    <div className="HeaderContainer">
+			<div className="ContactInfo">
+				
+			</div>
+			<div className="LogoContainer"> 
+				<img className="Logo" src={require('../Figma Components/ReboLogoWhite.png')} alt="LogoRebo"/>
+			</div>
+			
+    </div>
+    );
+  };
+  
+  export default HeaderBar;
