9718518a78d7a0e53872de07d6e49c950e627b51
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-09-23 00:14:06 -0600

    Changed WebTitle

--- src/Components/Body/BodyStructure.tsx
+++ src/Components/Body/BodyStructure.tsx
@@ -8,8 +8,12 @@ const BodyStructure: react.FC<BodyStructureprops>=() =>{
    return (
 		  <div className="FullBody">
 				<div className="TopBody">
-					<div className="Top1"></div>
-					<div className="Top2"></div>
+					<div className="Top1">
+					 <img className="LogoReboTop" src={require('../Figma Components/ReboTopSection.png')} />
+					</div>
+					<div className="Top2">
+						Lo sentimos, este sitio web aún se encuentra bajo construcción.
+					</div>
 					<div className="Top3"></div>
 
 				</div>
