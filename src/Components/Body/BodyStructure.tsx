1c79a54ac8bf9b41413f3884255742b12769dc37
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-09-28 12:28:13 -0600

    Changed bootstrap components for body and header

--- src/Components/Body/BodyStructure.tsx
+++ src/Components/Body/BodyStructure.tsx
@@ -1,36 +1,36 @@
-import react from "react";
+import React from "react";
 import './BodyStructure.css';
 import BodyTop1Button from "../Buttons/BodyTop1Button";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Asegúrate de importar Bootstrap
 
-interface BodyStructureprops {
-
-}
-const BodyStructure: react.FC<BodyStructureprops>=() =>{
-   return (
-		  <div className="FullBody">
-				<div className="TopBody">
-					<div className="Top1">
-					 <img className="LogoReboTop" src={require('../Figma Components/ReboTopSection.png')} alt="" />
-					</div>
-					<div className="Top2">
-						Lo sentimos, este sitio web aún se encuentra bajo construcción. Comúnicate con nosotros en los siguientes enlaces:
-					</div>
-					<div className="Top3">
-						<div className="Button1Top3">
-							<BodyTop1Button label="contacto@rebo.mx" ContactLink="mailto:contacto@rebo.mx" ButtonColor="rgba(255, 113, 0, 1)" TextColor="white"/>
-						</div>
-						<div className="Button2Top3">
-							<BodyTop1Button label="edgar.mtz@rebo.mx" ContactLink="mailto:edgar.mtz@rebo.mx" ButtonColor="white" TextColor="rgba(255, 113, 0, 1)"/>
-						</div>
-						<div className="Button3Top3">
-							<BodyTop1Button label="55 6158 5215" ContactLink="https://wa.me/525561585215" ButtonColor="rgba(255, 113, 0, 1)" TextColor="white"/>
-						</div>
-					</div>
-
-						
-				</div>
-
+const BodyStructure: React.FC = () => {
+  return (
+    <div className="FullBody container-fluid">
+      <div className="TopBody">
+        <div className="Top1 row justify-content-left">
+          <div className="col text-center">
+            <img className="LogoReboTop img-fluid" src={require('../Figma Components/ReboTopSection.png')} alt="Logo" />
+          </div>
+        </div>
+        <div className="Top2 row justify-content-center text-center py-3 bg-dark text-white">
+          <div className="col">
+            Lo sentimos, este sitio web aún se encuentra bajo construcción. Comúnicate con nosotros en los siguientes enlaces:
+          </div>
+        </div>
+        <div className="Top3 row justify-content-center mt-4">
+          <div className="col-12 col-md-auto">
+            <BodyTop1Button label="contacto@rebo.mx" ContactLink="mailto:contacto@rebo.mx" ButtonColor="rgba(255, 113, 0, 1)" TextColor="white" />
+          </div>
+          <div className="col-12 col-md-auto">
+            <BodyTop1Button label="edgar.mtz@rebo.mx" ContactLink="mailto:edgar.mtz@rebo.mx" ButtonColor="white" TextColor="rgba(255, 113, 0, 1)" />
+          </div>
+          <div className="col-12 col-md-auto">
+            <BodyTop1Button label="55 6158 5215" ContactLink="https://wa.me/525561585215" ButtonColor="rgba(255, 113, 0, 1)" TextColor="white" />
+          </div>
+        </div>
       </div>
+    </div>
   );
 };
+
 export default BodyStructure;
--- src/Components/Header/HeaderBar.tsx
+++ src/Components/Header/HeaderBar.tsx
@@ -1,28 +1,23 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Asegúrate de importar Bootstrap
 import './HeaderBar.css';
 
+interface HeaderBarprops {}
 
-interface HeaderBarprops{
-  
-}
 const HeaderBar: React.FC<HeaderBarprops> = () => {
-
 	return (
-    <div className="HeaderContainer">
-			<div className="ContactInfo">
-				
+		<div className="container-fluid p-0">
+			<div className="ContactInfo bg-dark d-flex align-items-center">
+				{/* Información de contacto aquí */}
 			</div>
-			<div className="LogoContainer"> 
-				<img className="Logo" src={require('../Figma Components/ReboLogoWhite.png')} alt="LogoRebo"/>
-				<div className="ButtonContainer">
-				
-
+			<div className="LogoContainer d-flex align-items-center justify-content-between bg-gradient">
+				<img className="Logo img-fluid" src={require('../Figma Components/ReboLogoWhite.png')} alt="LogoRebo"/>
+				<div className="ButtonContainer d-flex gap-3">
+					{/* Botones o enlaces aquí */}
 				</div>
-				
 			</div>
-			
-    </div>
-    );
-  };
-  
-  export default HeaderBar;
+		</div>
+	);
+};
+
+export default HeaderBar;
