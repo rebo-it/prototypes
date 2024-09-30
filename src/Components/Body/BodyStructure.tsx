7305f2eb56fcc3464ebb37328024a9072a2915a8
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-09-30 17:54:36 -0600

    changed size of visible devices

--- src/Components/Body/BodyStructure.tsx
+++ src/Components/Body/BodyStructure.tsx
@@ -12,17 +12,18 @@ const BodyStructure: React.FC = () => {
             <img className="LogoReboTop img-fluid" src={require('../Figma Components/ReboTopSection.png')} alt="Logo" />
           </div>
         </div>
-        <div className="Top2 row justify-content-center text-center py-3 bg-dark text-white">
-		Lo sentimos, este sitio web aún se encuentra bajo construcción. Comúnicate con nosotros en los siguientes enlaces:
+        <div className="Top2 row justify-content-center text-center py-3 bg-dark text-white"> 
+			<p className="h2">Lo sentimos, este sitio web aún se encuentra bajo construcción. Comúnicate con nosotros en los siguientes enlaces:</p>
+		
         </div>
         <div className="Top3 row justify-content-center mt-4">
-          <div className="col-12 col-md-auto">
+          <div className="col-12 col-sm-auto">
             <BodyTop1Button label="contacto@rebo.mx" ContactLink="mailto:contacto@rebo.mx" ButtonColor="rgba(255, 113, 0, 1)" TextColor="white" />
           </div>
-          <div className="col-12 col-md-auto">
+          <div className="col-12 col-sm-auto">
             <BodyTop1Button label="edgar.mtz@rebo.mx" ContactLink="mailto:edgar.mtz@rebo.mx" ButtonColor="white" TextColor="rgba(255, 113, 0, 1)" />
           </div>
-          <div className="col-12 col-md-auto">
+          <div className="col-12 col-sm-auto">
             <BodyTop1Button label="55 6158 5215" ContactLink="https://wa.me/525561585215" ButtonColor="rgba(255, 113, 0, 1)" TextColor="white" />
           </div>
         </div>
