f5ea7e09ffe6306e68d4a65b296e18369873d09d
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-09-30 17:03:52 -0600

    changed button to bootstrap settings

--- src/Components/Buttons/BodyTop1Button.tsx
+++ src/Components/Buttons/BodyTop1Button.tsx
@@ -1,24 +1,20 @@
-import react from "react";
-import './BodyTop1Button.css'
-interface BodyTop1Buttonprops{
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Asegúrate de importar Bootstrap en tu proyecto
+import './BodyTop1Button.css';
+
+interface BodyTop1Buttonprops {
     label: string;
     ContactLink: string;
     ButtonColor: string;
     TextColor: string;
-
-
 }
-const BodyTop1Button: react.FC<BodyTop1Buttonprops>=({ label, ContactLink, ButtonColor, TextColor}) =>{
-    return (
-        <button className="Top1Button" style={{backgroundColor: ButtonColor, }}>
-            <a className="GeneralLink" href={ContactLink} target="_blank" rel="noreferrer">
-                <div className="ButtonInformation"> 
-                    <div className="ButtonText"><p className="Paragraph" style={{color:TextColor}}> {label}</p></div>
-                </div>
-            </a>
-            
-        </button>
-    );
 
+const BodyTop1Button: React.FC<BodyTop1Buttonprops> = ({ label, ContactLink, ButtonColor, TextColor }) => {
+    return (
+        <a href={ContactLink} target="_blank" rel="noreferrer" className="btn" style={{ backgroundColor: ButtonColor, color: TextColor }}>
+            {label}
+        </a>
+    );
 };
+
 export default BodyTop1Button;
