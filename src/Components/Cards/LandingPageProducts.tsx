272faafb5a454371c878e0e1601c87269ec11a39
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-10-14 19:09:24 -0600

    changed casco 1

--- src/Components/Cards/LandingPageProducts.tsx
+++ src/Components/Cards/LandingPageProducts.tsx
@@ -10,7 +10,7 @@ interface LandingPageProductsprops{
 const LandingPageProducts: react.FC<LandingPageProductsprops>=({ label, ContactLink,  label2 }) =>{
     return (
 <div className="card sm" >
-  <img src={require("/WIP/prototypes/src/images/casco1.png")} className="ImageEquipment card-img-top img-fluid" alt="..."/>
+  <img src={require("/WIP/prototypes/src/Components/Figma Components/casco1.png")} className="ImageEquipment card-img-top img-fluid" alt="..."/>
   <div className="card-body">
     <h5 className="ProductName card-title text-white" style={{fontSize:'calc(1.25rem+0.5vw)'}}>{label}</h5>
     <a href={ContactLink} className="CardLink btn " >{label2} </a>
