7eb8d1d10c515c2f9e180cc0f00a8fae9b0e5ce2
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-10-18 21:25:08 -0600

    changed css

--- src/Components/Cards/MedalHolder.tsx
+++ src/Components/Cards/MedalHolder.tsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import QualityCards from './QualityCard';
+import './MedalHolder.css'
 
 function MedalHolder() {
   return (
