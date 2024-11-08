8f771ac1d8749afa0b3c17c419edb2ad0c5bf07d
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-11-07 22:09:48 -0600

    fixed issue

--- src/Components/Header and Footer/HeaderBar.tsx
+++ src/Components/Header and Footer/HeaderBar.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
+
 import './HeaderBar.css';
 import HeaderButton from "../Buttons/HeaderButton";
 import { Link } from 'react-router-dom';
