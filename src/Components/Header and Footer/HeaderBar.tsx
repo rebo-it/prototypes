cfe3d9dd9307a8a95a10b9ec2aac28eb069b2ee8
Therealsamumanz <smanzog1900@alumno.ipn.mx>
2024-11-18 21:25:49 -0600

    fixed issues and unresolved problems

--- src/Components/Header and Footer/HeaderBar.tsx
+++ src/Components/Header and Footer/HeaderBar.tsx
@@ -25,12 +25,12 @@ const HeaderBar: React.FC<HeaderBarprops> = () => {
 						<Nav className="TopNav navbar-nav w-100">
 							<div className="d-flex direction-row" >
 							<Nav.Item className="nav-item" style={{paddingLeft:'10px'}}>
-								<Nav.Link href="https://api.whatsapp.com/send/?phone=5215561585215&text&type=phone_number&app_absent=0">
+								<Nav.Link  href="https://api.whatsapp.com/send/?phone=5215561585215&text&type=phone_number&app_absent=0" target="_blank" active={false}>
 									<i className="bi bi-whatsapp"></i> 55 615 852 15
 								</Nav.Link>
 							</Nav.Item>
 							<Nav.Item className="nav-item justify-content-end">
-								<Nav.Link href="mailto:contacto@rebo.mx">
+								<Nav.Link href="mailto:contacto@rebo.mx" target="_blank" active={false}>
 									<i className="bi bi-envelope"></i> contacto@rebo.mx
 								</Nav.Link>
 							</Nav.Item>
@@ -38,7 +38,7 @@ const HeaderBar: React.FC<HeaderBarprops> = () => {
 							</div>
 							<div className="d-flex direction-row justify-content-end flex-grow-1 " style={{minWidth:'none', paddingRight:'20px'}}>
 								<Nav.Item className="nav-item d-none d-lg-block " >
-									<Nav.Link href="https://www.google.com/maps/place/El+Rosario/@19.5046653,-99.2000975,15z/data=!4m2!3m1!1s0x0:0x8a95c0f781b015f2?sa=X&ved=1t:2428&ictx=111">
+									<Nav.Link href="https://www.google.com/maps/place/El+Rosario/@19.5046653,-99.2000975,15z/data=!4m2!3m1!1s0x0:0x8a95c0f781b015f2?sa=X&ved=1t:2428&ictx=111" target="_blank" active={false}>
 										<i className="bi bi-geo-alt"></i> Ubicación
 									</Nav.Link>
 								</Nav.Item>
